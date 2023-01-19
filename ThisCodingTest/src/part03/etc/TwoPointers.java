820af7dd2cf36405a273dc453d92093d232a334a
suhyun <qkrtngus116@naver.com>
2023-01-19 15:06:06 +0900

    투포인터(부분수열찾기)

---
+++ ThisCodingTest/src/part03/etc/TwoPointers.java
@@ -0,0 +1,28 @@
+package ThisCodingTest.src.part03.etc;
+
+public class TwoPointers {
+
+    static int n = 5;
+    static int m = 5;
+    static int[] a = {1,2,3,2,5};
+    public static void main(String[] args) {
+        int cnt = 0;
+        int intervalSum = 0;
+        int end = 0;
+
+        //start를 차례대로 증가시키며 반복
+        for(int start = 0; start <n; start++){
+            //end를 가능한만큼 이동시키기
+            while(intervalSum < m && end < n){
+                intervalSum += a[end];
+                end += 1;
+                //System.out.println(intervalSum + " , " + end);
+            }
+            if(intervalSum == m){
+                cnt += 1;
+            }
+            intervalSum -= a[start];
+        }
+        System.out.println(cnt);
+    }
+}
